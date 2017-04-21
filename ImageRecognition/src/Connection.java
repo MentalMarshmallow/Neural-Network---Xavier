59631a24de692903d890e84570a271526e875d79
Vimal Jain <vimalj57@yahoo.ie>
2017-04-21 15:53:49 +0100

    Completed a basic node

---
+++ ImageRecognition/src/Connection.java
@@ -0,0 +1,6 @@
+/*
+ * This class is the connection between any two nodes
+ */
+public class Connection {
+
+}
--- ImageRecognition/src/Node.java
+++ ImageRecognition/src/Node.java
@@ -1,8 +1,22 @@
+/*
+ * This is each node in the input, output and hidden layers
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Node {
 	
 	static int counter=0;// This is the id of each of the neurons that will increment each time a new neuron is created
+	final public int id;
 	double value;
 	
 	ArrayList<Connection> Connections = new ArrayList<Connection>();
-	Connection
+	HashMap<Integer,Connection> findConnection = new HashMap<Integer,Connection>();
+	
+	public Node()
+	{
+		id=counter;
+		counter++;
+	}
+	
 }
